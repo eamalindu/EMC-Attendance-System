18e3ae4e9af491d9061db7191e44909da6e4f4b5
Malindu <eamalindu@gmail.com>
2025-08-24 23:06:31 +0530

    edited

--- dashbaord.php
+++ dashbaord.php
@@ -5,12 +5,51 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Mark Attendance</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/aquawolf04/font-awesome-pro@5cd1511/css/all.css">
     <link rel="stylesheet" href="css/all.css">
 </head>
 <body>
 <div class="container p-5">
     <h2 >Dashboard</h2>
     <p class="mb-3">Welcome Back </p>
+
+    <div class="row">
+        <div class="col-3">
+            <div class="card border-success">
+                <div class="card-body">
+                    <i class="fas fa-user-graduate"></i>
+                </div>
+                <div class="card-footer text-center fw-bold border-success bg-success-subtle">
+                    Mark Attendance
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card">
+                <div class="card-body"> </div>
+                <div class="card-footer text-center text-muted">
+                    Update Barcode
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card">
+                <div class="card-body"> </div>
+                <div class="card-footer text-center text-muted">
+                    Update Barcode
+                </div>
+            </div>
+        </div>
+        <div class="col-3">
+            <div class="card">
+                <div class="card-body"> </div>
+                <div class="card-footer text-center text-muted">
+                    Mark Attendance
+                </div>
+            </div>
+        </div>
+    </div>
+
 
 </div>
 </body>
