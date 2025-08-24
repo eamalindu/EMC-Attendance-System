df8266cc493f191c0591cb8974b8e7ad50813a64
Malindu <eamalindu@gmail.com>
2025-08-24 21:32:16 +0530

    html added

--- dashbaord.php
+++ dashbaord.php
@@ -1,1 +1,17 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Mark Attendance</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/all.css">
+</head>
+<body>
+<div class="container p-5">
+    <h2 >Dashboard</h2>
+    <p class="mb-3">Welcome Back </p>
+
+</div>
+</body>
+</html>
