4e1839ed5a5da3b6532ff3e74d829e5e64dbddc3
Malindu <eamalindu@gmail.com>
2025-08-25 19:33:24 +0530

    files linked

--- update-barcode.php
+++ update-barcode.php
@@ -89,8 +89,10 @@
 
 
     </div>
+    <button class="btn btn-secondary mt-4 mx-auto d-block"><a href="dashboard.php" class="text-white text-decoration-none">Back To Dashboard</a></button>
+
 </div>
-</div>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q"
         crossorigin="anonymous"></script>
