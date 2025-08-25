f58eaec93da738e2a06ca02f59697422ca794cf9
Malindu <eamalindu@gmail.com>
2025-08-25 22:56:35 +0530

    edited

--- dashboard.php
+++ dashboard.php
@@ -231,9 +231,7 @@ require_once "config.php";
 <div class="logout">
     <button class="btn btn-danger btn-sm" onclick="logout()">Logout</button>
 </div>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q"
-        crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
 <script src="customModal_V2/resources/js/customModal@2.0.min.js"></script>
 <script src="logout.js"></script>
 </body>
