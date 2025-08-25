38239befc5233fd47708aee71ccad7020bcdb8aa
Malindu <eamalindu@gmail.com>
2025-08-25 19:27:31 +0530

    file renamed

--- attendance.php
+++ attendance.php
@@ -70,6 +70,7 @@
             </div>
         </div>
     </div>
+        <button class="btn btn-secondary mt-4 mx-auto d-block"><a href="dashboard.php" class="text-white text-decoration-none">Back To Dashboard</a></button>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q"
