6203aeb5926473ae73b3ed1f887032bfb3862ab0
Malindu <eamalindu@gmail.com>
2025-08-26 10:11:40 +0530

    loader added

--- attendance-history.php
+++ attendance-history.php
@@ -31,8 +31,12 @@ if (isset($_GET['reg'])) {
 
     <link rel="stylesheet" href="https://cdn.datatables.net/2.3.2/css/dataTables.dataTables.min.css">
     <link rel="stylesheet" href="https://cdn.datatables.net/2.3.2/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="css/loader.css">
 </head>
 <body>
+<div class="preloader w-100 min-vh-100">
+    <div class="loader"></div>
+</div>
 <div class="container p-5">
 
     <div class="row m-0 mb-4">
@@ -133,5 +137,6 @@ if (isset($_GET['reg'])) {
     });
 
 </script>
+<script src="loader.js"></script>
 </body>
 </html>
--- attendance.php
+++ attendance.php
@@ -7,8 +7,12 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
     <link rel="stylesheet" href="css/all.css">
+    <link rel="stylesheet" href="css/loader.css">
 </head>
 <body>
+<div class="preloader w-100 min-vh-100">
+    <div class="loader"></div>
+</div>
 <div class="container p-5">
     <h2 class="mb-3">Mark Attendance</h2>
     <div class="row mb-4">
@@ -77,5 +81,6 @@
         crossorigin="anonymous"></script>
 <script src="customModal_V2/resources/js/customModal@2.0.min.js"></script>
 <script src="app.js"></script>
+<script src="loader.js"></script>
 </body>
 </html>
