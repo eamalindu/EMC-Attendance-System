e9b6e06c288e0fa535e62a7c22d31b74e84c7d24
Malindu <eamalindu@gmail.com>
2025-08-26 11:48:21 +0530

    edited

--- generateReport.php
+++ generateReport.php
@@ -20,6 +20,20 @@ if(isset($_GET["SelectedDate"]) || isset($_GET["SelectedBatch"])) {
         }
 
     }
+    if(isset($_GET["SelectedBatch"])) {
+        $selectedBatch  = $_GET["SelectedBatch"];
+        $sqlBatchOnly = "SELECT reg_id,batch_id,timestamp,addedBY FROM attendance where batch_id = '$selectedBatch'";
+        $resultBatchOnly = $conn->query($sqlBatchOnly);
+        if ($resultBatchOnly) {
+            if ($resultBatchOnly->num_rows > 0) {
+                echo json_encode($resultBatchOnly->fetch_assoc());
+
+            }
+            else{
+                echo json_encode(["error" => "No student found"]);
+            }
+        }
+    }
 
 }
 
