e8c03c8800cc39ecf69e29209fe1b408d27db757
Malindu <eamalindu@gmail.com>
2025-08-26 13:57:58 +0530

    reports will now show today's attendance

--- report.js
+++ report.js
@@ -3,58 +3,72 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const today = new Date();
     inputDate.max = today.toISOString().slice(0, 10);
-})
 
-const generateReport = ()=>{
-
-    let selectedDate = date.value;
-    let selectedBatch = batch.value;
-
-    if(selectedBatch!=="" || selectedDate!=="") {
-
-        if(selectedDate!=="" && selectedBatch!==""){
-            //both selected
-            console.log("Both Selected");
-            fetch(`generateReport.php?SelectedDate=${encodeURIComponent(selectedDate)}&SelectedBatch=${encodeURIComponent(selectedBatch)}`, {
-                method: 'GET', headers: {'Accept': 'application/json'}
-            })
-                .then(response => response.json())
-                .then(data=>{
-                    console.log(data);
-                })
-        }
-
-        if(selectedDate!=="" && selectedBatch===""){
-            //only date
-            console.log("Date Selected");
-            fetch(`generateReport.php?SelectedDate=${encodeURIComponent(selectedDate)}`, {
-                method: 'GET', headers: {'Accept': 'application/json'}
-            })
-                .then(response => response.json())
-                .then(data=>{
-                    console.log(data)
-                    displayListDate = [{property:'reg_id',dataType:'text'},{property:'batch_id',dataType:'text'},{property:'timestamp',dataType:'text'},{property:'addedBY',dataType:'text'}];
-                    JsonToTable(data,tblReport,displayListDate);
-                })
-
-        }
-
-        if(selectedDate==="" && selectedBatch!==""){
-            //only batch
-            console.log("Batch Selected");
-
-        }
-
-    }
-    else{
-        showCustomModal("Please select date or batch","warning");
-    }
-
-
-
-}
-
-document.querySelector('#btnClear').addEventListener('click', ()=>{
-    date.value = "";
-    batch.value = "";
+    fetch(`generateReport.php?SelectedDate=${encodeURIComponent(today.toISOString().slice(0, 10))}`, {
+        method: 'GET', headers: {'Accept': 'application/json'}
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            displayListDate = [{property: 'reg_id', dataType: 'text'}, {
+                property: 'batch_id',
+                dataType: 'text'
+            }, {property: 'timestamp', dataType: 'text'}, {property: 'addedBY', dataType: 'text'}];
+            JsonToTable(data, tblReport, displayListDate);
+        })
 });
+
+    const generateReport = () => {
+
+        let selectedDate = date.value;
+        let selectedBatch = batch.value;
+
+        if (selectedBatch !== "" || selectedDate !== "") {
+
+            if (selectedDate !== "" && selectedBatch !== "") {
+                //both selected
+                console.log("Both Selected");
+                fetch(`generateReport.php?SelectedDate=${encodeURIComponent(selectedDate)}&SelectedBatch=${encodeURIComponent(selectedBatch)}`, {
+                    method: 'GET', headers: {'Accept': 'application/json'}
+                })
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log(data);
+                    })
+            }
+
+            if (selectedDate !== "" && selectedBatch === "") {
+                //only date
+                console.log("Date Selected");
+                fetch(`generateReport.php?SelectedDate=${encodeURIComponent(selectedDate)}`, {
+                    method: 'GET', headers: {'Accept': 'application/json'}
+                })
+                    .then(response => response.json())
+                    .then(data => {
+                        console.log(data)
+                        displayListDate = [{property: 'reg_id', dataType: 'text'}, {
+                            property: 'batch_id',
+                            dataType: 'text'
+                        }, {property: 'timestamp', dataType: 'text'}, {property: 'addedBY', dataType: 'text'}];
+                        JsonToTable(data, tblReport, displayListDate);
+                    })
+
+            }
+
+            if (selectedDate === "" && selectedBatch !== "") {
+                //only batch
+                console.log("Batch Selected");
+
+            }
+
+        } else {
+            showCustomModal("Please select date or batch", "warning");
+        }
+
+
+    }
+
+    document.querySelector('#btnClear').addEventListener('click', () => {
+        date.value = "";
+        batch.value = "";
+    });
