34a7ba66b219c750a606f1936802459ff8fb0ae3
Malindu <eamalindu@gmail.com>
2025-08-26 12:34:20 +0530

    both parameters implemented

--- generateReport.php
+++ generateReport.php
@@ -35,6 +35,23 @@ if(isset($_GET["SelectedDate"]) || isset($_GET["SelectedBatch"])) {
         }
     }
 
+    if(isset($_GET["SelectedDate"]) && isset($_GET["SelectedBatch"])) {
+        $selectedDate  = $_GET["SelectedDate"];
+        $selectedBatch  = $_GET["SelectedBatch"];
+
+        $sqlBothSelected = "reg_id,batch_id,timestamp,addedBY FROM attendance where batch_id = '$selectedBatch'and day(timestamp) = '$selectedDate' ";
+        $resultBothSelected = $conn->query($sqlBothSelected);
+        if ($resultBothSelected) {
+            if ($resultBothSelected->num_rows > 0) {
+                echo json_encode($resultBothSelected->fetch_assoc());
+
+            }
+            else{
+                echo json_encode(["error" => "No student found"]);
+            }
+        }
+    }
+
 }
 
 
