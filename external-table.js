69b9d6cf87449ac291f8618ac86365de9c828693
Malindu <eamalindu@gmail.com>
2025-08-26 13:09:12 +0530

    function start to implement

--- external-table.js
+++ external-table.js
@@ -1,4 +1,19 @@
-const JsonToTable = (data) => {
+const JsonToTable = (data,tableID) => {
 
+    const tbody = tabledID.children[1];
+    //clear the table body
+    tbody.innerHTML = '';
+
+    if(data.length!==0) {
+
+    }
+    else{
+        const tableTR = document.createElement('tr');
+        const  tableTD = document.createElement('td');
+        tableTD.colSpan = (displayPropertyList.length+2);
+        tableTD.innerText = 'No Records Found!';
+        tableTR.appendChild(tableTD)
+        tbody.appendChild(tableTR);
+    }
 
 }
