51849eb8f361b1f87e63ff9ace982ef14bc0fc58
Malindu <eamalindu@gmail.com>
2025-08-31 09:00:50 +0530

    title updated

--- manage-students.php
+++ manage-students.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Update Barcode</title>
+    <title>Manage Student</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
     <link rel="stylesheet" href="css/all.css">
@@ -14,7 +14,7 @@
     <div class="loader"></div>
 </div>
 <div class="container p-5">
-    <h2 class="mb-3">Update Barcode (Student Access Card)</h2>
+    <h2 class="mb-3">Manage Students</h2>
 
 
 </div>
