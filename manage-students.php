3e7a74a13c735a43b31d14569e86f8658d26d3ff
Malindu <eamalindu@gmail.com>
2025-08-31 09:00:12 +0530

    js linked

--- manage-students.php
+++ manage-students.php
@@ -18,5 +18,10 @@
 
 
 </div>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q"
+        crossorigin="anonymous"></script>
+<script src="customModal_V2/resources/js/customModal@2.0.min.js"></script>
+<script src="js/loader.js"></script>
 </body>
 </html>
