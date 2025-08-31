52aa2ab1f8760c67e147bfa236c3c8f771caf5d3
Malindu <eamalindu@gmail.com>
2025-08-31 08:58:58 +0530

    boilerplate coded added

--- manage-students.php
+++ manage-students.php
@@ -1,1 +1,22 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Update Barcode</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/all.css">
+    <link rel="stylesheet" href="css/loader.css">
+</head>
+<body>
+<div class="preloader w-100 min-vh-100">
+    <div class="loader"></div>
+</div>
+<div class="container p-5">
+    <h2 class="mb-3">Update Barcode (Student Access Card)</h2>
+
+
+</div>
+</body>
+</html>
