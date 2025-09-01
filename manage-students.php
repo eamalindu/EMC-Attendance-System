dc2a893ddbe038b2753222551daa61d4520448fa
Malindu <eamalindu@gmail.com>
2025-09-01 21:36:21 +0530

    dashboard linked

--- manage-students.php
+++ manage-students.php
@@ -66,7 +66,7 @@
         echo "</tbody></table>";
     }
     ?>
-
+    <button class="btn btn-secondary mt-4 mx-auto d-block"><a href="dashboard.php" class="text-white text-decoration-none">Back To Dashboard</a></button>
 </div>
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"
         integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
