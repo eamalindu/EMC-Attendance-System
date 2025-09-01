6375544a4dd9e2943eea65c5badc52dd6abdda1a
Malindu <eamalindu@gmail.com>
2025-09-01 21:25:54 +0530

    new function added

--- js/manageStudents.js
+++ js/manageStudents.js
@@ -6,5 +6,19 @@ const deleteStudent = (button) => {
 
     let reg = button.getAttribute("data-reg");
     console.log(reg);
-    console.log(button);
+}
+
+const getStudent= (button)=>{
+
+    let reg = button.getAttribute("data-reg");
+    fetch(`getStudent.php?reg=${encodeURIComponent(reg)}`, {
+        method: 'GET', headers: {'Accept': 'application/json'}
+    })
+    .then(response => response.json())
+    .then(
+        data=>{
+            console.log(data);
+        }
+    )
+
 }
