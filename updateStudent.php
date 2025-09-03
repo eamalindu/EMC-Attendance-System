5e03958ac6da53e689e4389581adf366e5d63c8b
Malindu <eamalindu@gmail.com>
2025-09-03 17:14:53 +0530

    update student implemented

--- updateStudent.php
+++ updateStudent.php
@@ -1,1 +1,28 @@
 <?php
+
+require_once "config.php";
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $reg = $_POST['reg'];
+    $name = $_POST['name'];
+    $batch = $_POST['batch'];
+    $contact = $_POST['contact'];
+    $pContact = $_POST['pContact'];
+    $status = $_POST['status'];
+
+    $stmt = "UPDATE student SET name= ?, batch= ?, contact =?, pStatus = ?,sStatus = ? WHERE reg = ? ";
+    $stmt -> bind_param("ssssss", $name, $batch, $contact, $pContact, $status, $reg);
+    $stmt-> execute();
+
+    if($stmt->execute()){
+        echo "Ok";
+
+    } else {
+        echo "failed: " . $stmt->error;
+    }
+
+    $stmt->close();
+    $conn->close();
+
+
+}
