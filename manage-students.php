2956fb49c792d19b425f3131c03e6b15760d16c8
Malindu <eamalindu@gmail.com>
2025-09-03 08:35:51 +0530

    offcanvas added for the student view button

--- manage-students.php
+++ manage-students.php
@@ -57,7 +57,7 @@
             echo "<td class='text-center'>0" . $row['contact'] . "</td>";
             echo "<td class='text-center'>0".$row["pStatus"]."</td>";
             echo "<td class='text-center'>".$row["sStatus"]."</td>";
-            echo "<td><button class='btn btn-success btn-sm' onclick='getStudent(this)' data-reg='".htmlspecialchars($row['reg'])."'><i class='fa-solid fa-eye'></i></button>
+            echo "<td><button class='btn btn-success btn-sm' type='button' data-bs-toggle='offcanvas' data-bs-target='#offcanvasExample' aria-controls='offcanvasExample' onclick='getStudent(this)' data-reg='".htmlspecialchars($row['reg'])."'><i class='fa-solid fa-eye'></i></button>
                   <button class='btn btn-outline-danger btn-sm' onclick='deleteStudent(this)' data-reg='".htmlspecialchars($row['reg'])."'><i class='fa-solid fa-trash'></i></button>
                     </td>";
             $rowIndex++;
@@ -67,6 +67,29 @@
     }
     ?>
     <button class="btn btn-secondary mt-4 mx-auto d-block"><a href="dashboard.php" class="text-white text-decoration-none">Back To Dashboard</a></button>
+</div>
+
+<!--offcanvas-->
+<div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasExample" aria-labelledby="offcanvasExampleLabel">
+    <div class="offcanvas-header">
+        <h5 class="offcanvas-title" id="offcanvasExampleLabel">Offcanvas</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+    </div>
+    <div class="offcanvas-body">
+        <div>
+            Some text as placeholder. In real life you can have the elements you have chosen. Like, text, images, lists, etc.
+        </div>
+        <div class="dropdown mt-3">
+            <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown">
+                Dropdown button
+            </button>
+            <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Action</a></li>
+                <li><a class="dropdown-item" href="#">Another action</a></li>
+                <li><a class="dropdown-item" href="#">Something else here</a></li>
+            </ul>
+        </div>
+    </div>
 </div>
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"
         integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
