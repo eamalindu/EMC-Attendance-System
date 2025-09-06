28daa199ec754c8fa50aa501242912ec27c69f57
Malindu <eamalindu@gmail.com>
2025-09-06 22:36:29 +0530

    implementation started

--- deleteStudent.php
+++ deleteStudent.php
@@ -2,7 +2,15 @@
 session_start();
 require_once "config.php";
 
+$loggedInUser = $_SESSION["username"];
 
-echo "Service Not implemented";
+if ($loggedInUser == "admin") {
+    echo "Service Not implemented";
+}
+else{
+    echo "Sorry You are not authorized to<br>perform this action!<br><br>This attempt has been recorded";
+}
+
+
 
 
