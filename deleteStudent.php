cbfb8f1db456c2c28fcbacdd9225274d38192973
Malindu <eamalindu@gmail.com>
2025-09-07 23:07:56 +0530

    implementation finished

--- deleteStudent.php
+++ deleteStudent.php
@@ -4,11 +4,23 @@ require_once "config.php";
 
 $loggedInUser = $_SESSION["username"];
 
-if ($loggedInUser == "admin") {
-    echo "Service Not implemented";
-}
-else{
-    echo "Sorry You are not authorized to<br>perform this action!<br><br>This attempt has been recorded";
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $reg = $_POST['reg'];
+
+    if ($loggedInUser == "admin") {
+        echo "Service Not implemented";
+
+        $stmt = $conn->prepare("UPDATE student SET sStatus = 'Deleted' WHERE reg = ?");
+        $stmt->bind_param("s", $reg);
+        if ($stmt->execute()) {
+            echo "OK";
+        }
+        else{
+            echo "failed: " . $stmt->error;
+        }
+    } else {
+        echo "Sorry You are not authorized to<br>perform this action!<br><br>This attempt has been recorded";
+    }
 }
 
 
