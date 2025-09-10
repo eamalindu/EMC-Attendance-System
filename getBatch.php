97632cc8d927b882623c9c007d0ff7b5560e45db
Malindu <eamalindu@gmail.com>
2025-09-10 18:03:48 +0530

    backend code implementation started

--- getBatch.php
+++ getBatch.php
@@ -1,1 +1,11 @@
 <?php
+session_start();
+header("Content-Type: application/json");
+
+require_once "config.php";
+
+if (isset($_GET['batch'])) {
+    $reg = $conn->real_escape_string($_GET['batch']);
+    $_SESSION['batch'] = $reg;
+
+}
