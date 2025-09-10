5399e823bf6df5a5da223e934fc8c9ef13fce265
Malindu <eamalindu@gmail.com>
2025-09-10 18:02:29 +0530

    getBatch function implementation started

--- js/manageBatches.js
+++ js/manageBatches.js
@@ -7,8 +7,16 @@ const refreshBatch = ()=>{
 
 }
 
-const getBatch = ()=>{
-
+const getBatch = (button)=>{
+    let batchName = button.getAttribute("data-reg");
+    fetch(`getBatch.php?reg=${encodeURIComponent(batchName)}`, {
+        method: 'GET', headers: {'Accept': 'application/json'}
+    })
+        .then(response => response.json())
+        .then(
+            data=> {
+                console.log(data);
+            })
 }
 
 const completeBatch = ()=>{
