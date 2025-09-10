98f0c13e4f7c8c4d73d754383bc31b7b8c9f5b51
Malindu <eamalindu@gmail.com>
2025-09-10 12:57:15 +0530

    log added

--- logout.php
+++ logout.php
@@ -1,5 +1,13 @@
 <?php
 session_start();
+require_once "config.php";
+
+$username = $_SESSION['username'];
+$stmt_log = $conn->prepare("INSERT INTO log (user,description,created) VALUES (?,?,NOW())");
+$log = "User Logged Out ";
+$stmt_log->bind_param("ss", $username, $log);
+$stmt_log->execute();
+
 
 // Unset all session variables
 $_SESSION = array();
