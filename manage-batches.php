278d10a82c9c398e9c61978f5bfabd73f2036937
Malindu <eamalindu@gmail.com>
2025-09-10 15:54:29 +0530

    basic code added

--- manage-batches.php
+++ manage-batches.php
@@ -1,1 +1,31 @@
-<?php
+<?php session_start();
+if (!isset($_SESSION['loggedIn']) || $_SESSION['loggedIn'] !== true) {
+    header("Location: index.php");
+    exit();
+}
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Manage Student</title>
+    <link rel="icon" type="image/png" href="images/icon.png" sizes="256x256">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/aquawolf04/font-awesome-pro@5cd1511/css/all.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.3.2/css/dataTables.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.3.2/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="css/all.css">
+    <link rel="stylesheet" href="css/loader.css">
+</head>
+<body>
+<div class="preloader w-100 min-vh-100">
+    <div class="loader"></div>
+</div>
+<div class="container p-5">
+    <h2 class="mb-3">Manage Batches</h2>
+
+</div>
+</body>
+</html>
