5b1514f4ec67f19394429c50b15507377db49903
Malindu <eamalindu@gmail.com>
2025-09-10 16:34:55 +0530

    offcanvas added

--- manage-batches.php
+++ manage-batches.php
@@ -83,6 +83,70 @@ if (!isset($_SESSION['loggedIn']) || $_SESSION['loggedIn'] !== true) {
     <button class="btn btn-secondary mt-4 mx-auto d-block"><a href="dashboard.php" class="text-white text-decoration-none">Back To Dashboard</a></button>
 </div>
 
+<!--offcanvas-->
+<div class="offcanvas offcanvas-end" data-bs-backdrop="static" tabindex="-1" id="offcanvasExample"
+     aria-labelledby="offcanvasExampleLabel">
+    <div class="offcanvas-header justify-content-between">
+        <div>
+            <button type="button" class="btn btn-danger btn-red bg-red rounded-0 btn-sm" data-bs-dismiss="offcanvas" aria-label="Close"><i class="fa-solid fa-xmark"></i></button>
+            <button type="button" class="btn btn-secondary rounded-0 btn-sm" id="btnStudentReset" onclick="refreshStudent()"><i class="fa-solid fa-arrows-rotate"></i></button>
+        </div>
+        <h5 class="offcanvas-title " id="offcanvasExampleLabel">Manage Student Record</h5>
+    </div>
+    <div class="offcanvas-body">
+        <div class="mb-2">
+            <label for="sNAme" class="form-label">Student Name</label>
+            <input type="text" class="form-control rounded-0" id="sNAme" placeholder="John Doe">
+        </div>
+        <div class="mb-2">
+            <label for="sEID" class="form-label">Student EID</label>
+            <input type="text" class="form-control rounded-0" id="sEID" placeholder="EXXXXXX">
+        </div>
+        <div class="mb-2">
+            <label for="sREG" class="form-label">Student Registration</label>
+            <input type="text" class="form-control rounded-0" id="sREG" placeholder="00XXXX">
+        </div>
+        <div class="mb-2">
+            <label for="sBatch" class="form-label">Student Batch</label>
+            <select class="form-select rounded-0" id="sBatch" name="sBatch">
+                <?php
+                echo "<option value='' disabled selected>Select Batch</option>";
+                $sql = "SELECT DISTINCT name FROM batch order by name asc";
+                $result = mysqli_query($conn, $sql);
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "<option value='" . $row['name'] . "'>" . $row['name'] . "</option>";
+                }
+                ?>
+            </select>
+        </div>
+        <div class="mb-2">
+            <label for="sContact" class="form-label">Student Contact</label>
+            <input type="text" class="form-control rounded-0" id="sContact" placeholder="07X-XXXX-XXXX">
+        </div>
+        <div class="mb-2">
+            <label for="pContact" class="form-label">Parent Contact</label>
+            <input type="text" class="form-control rounded-0" id="pContact" placeholder="07X-XXXX-XXXX">
+        </div>
+        <div class="mb-4">
+            <label for="sStatus" class="form-label">Student Status</label>
+            <select class="form-select rounded-0" id="sStatus" name="sStatus">
+                <?php
+                echo "<option value='' disabled selected>Select Status</option>";
+                $sql = "SELECT DISTINCT sStatus FROM student order by sStatus asc";
+                $result = mysqli_query($conn, $sql);
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo "<option value='" . $row['sStatus'] . "'>" . $row['sStatus'] . "</option>";
+                }
+                ?>
+            </select>
+        </div>
+        <div class="mb-2 d-flex justify-content-between align-items-center gap-2">
+            <button class="btn btn-success w-50 btn-sm" id="btnStudentRegister">Register</button>
+            <button class="btn btn-warning w-50 btn-sm text-white" id="btnStudentUpdate" onclick="updateStudent();">Update</button>
+        </div>
+    </div>
+</div>
+
 <script src="https://code.jquery.com/jquery-3.7.1.min.js"
         integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
 <script src="https://cdn.datatables.net/2.3.2/js/dataTables.min.js"></script>
