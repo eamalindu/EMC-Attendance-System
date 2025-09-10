1c61719afd054292c6a044e62176c865a32267fc
Malindu <eamalindu@gmail.com>
2025-09-10 20:26:53 +0530

    new functions implemented to return ip and user agent of the current user

--- getIP.php
+++ getIP.php
@@ -1,19 +1,17 @@
 <?php
 function getUserIP() {
-    $ip = '';
-
     if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
-        // IP from shared internet
         $ip = $_SERVER['HTTP_CLIENT_IP'];
     } elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
-        // IP passed from proxy
         $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
-        // Sometimes it's a list of IPs, take the first one
-        $ip = explode(',', $ip)[0];
+        $ip = explode(',', $ip)[0]; // first IP if multiple
     } else {
-        // Direct IP from remote address
         $ip = $_SERVER['REMOTE_ADDR'];
     }
-
     return trim($ip);
+}
+
+function getUserAgent() {
+    return isset($_SERVER['HTTP_USER_AGENT']) ? $_SERVER['HTTP_USER_AGENT'] : 'Unknown';
+
 }
