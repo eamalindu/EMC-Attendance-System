0aaa50a94ff511e1148eae267535bcbdafa57fd9
Malindu <eamalindu@gmail.com>
2025-09-10 20:00:00 +0530

    getBatch function updated

--- js/manageBatches.js
+++ js/manageBatches.js
@@ -3,32 +3,74 @@ window.addEventListener('DOMContentLoaded', () => {
 });
 
 
-const refreshBatch = ()=>{
+const refreshBatch = () => {
 
 }
 
-const getBatch = (button)=>{
+const getBatch = (button) => {
     let batchName = button.getAttribute("data-reg");
     fetch(`getBatch.php?batch=${encodeURIComponent(batchName)}`, {
         method: 'GET', headers: {'Accept': 'application/json'}
     })
         .then(response => response.json())
-        .then(
-            data=> {
-                console.log(data);
-                oldBatch = data;
+        .then(data => {
+            console.log(data);
+            oldBatch = data;
 
-                bNAme.value = data.name;
-                bStartTime.value = data.startTime;
-                bEndTime.value = data.endTime;
+            bNAme.value = data.name;
+            bStartTime.value = data.startTime;
+            bEndTime.value = data.endTime;
 
-            })
+            //check schedule
+            if (data.monday === "1") {
+                bMonday.checked = true;
+            } else {
+                bMonday.checked = false;
+            }
+
+            if (data.tuesday === "1") {
+                bTuesday.checked = true;
+            } else {
+                bTuesday.checked = false;
+            }
+
+            if (data.wednesday === "1") {
+                bWednesday.checked = true;
+            } else {
+                bWednesday.checked = false;
+            }
+
+            if (data.thursday === "1") {
+                bThursday.checked = true;
+            } else {
+                bThursday.checked = false;
+            }
+
+            if (data.friday === "1") {
+                bFriday.checked = true;
+            } else {
+                bFriday.checked = false;
+            }
+
+            if (data.staurday === "1") {
+                bSaturday.checked = true;
+            } else {
+                bSaturday.checked = false;
+            }
+
+            if (data.sunday === "1") {
+                bSunday.checked = true;
+            } else {
+                bSunday.checked = false;
+            }
+
+        })
 }
 
-const completeBatch = ()=>{
+const completeBatch = () => {
 
 }
 
-const updateBatch = ()=>{
+const updateBatch = () => {
 
 }
