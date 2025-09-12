6262e1632c6842922669d1b0aacdb8650ee0cae3
Malindu <eamalindu@gmail.com>
2025-09-12 20:19:52 +0530

    sendWelcomeMail implemented

--- mailTemplate.php
+++ mailTemplate.php
@@ -1,16 +1,39 @@
 <?php
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
 
 require 'PHPMailer-6.10.0/src/Exception.php';
 require 'PHPMailer-6.10.0/src/PHPMailer.php';
 require 'PHPMailer-6.10.0/src/SMTP.php';
 
-
 function sendWelcomeMail($email)
 {
+    $mail = new PHPMailer(true);
 
+    try {
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'finalproject.eamalindu@gmail.com';
+        $mail->Password   = 'ludfsvhjbybjhnth';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port       = 465;
 
+        $mail->setFrom('finalproject.eamalindu@gmail.com', 'EMC-NIT Attendance System');
+        $mail->addAddress($email);
+
+        $mail->isHTML(true);
+        $mail->Subject = 'Welcome To EMC-NIT Attendance System';
+        $mail->Body    = 'This is a test email';
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        $mail->send();
+        echo 'OK';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
 }
 
+sendWelcomeMail("eamalindu@gmail.com");
 ?>
