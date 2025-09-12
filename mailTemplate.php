447e9475891b424de151e9f3a1e814ae2c6dada9
Malindu <eamalindu@gmail.com>
2025-09-12 19:48:18 +0530

    initial commit

---
+++ mailTemplate.php
@@ -0,0 +1,16 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+require 'PHPMailer-6.10.0/src/Exception.php';
+require 'PHPMailer-6.10.0/src/PHPMailer.php';
+require 'PHPMailer-6.10.0/src/SMTP.php';
+
+
+function sendWelcomeMail($email)
+{
+
+
+}
+
+?>
