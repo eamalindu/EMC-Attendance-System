1d7377ff7eead92a528b0c055336662c06a76d76
Malindu <eamalindu@gmail.com>
2025-09-13 12:56:51 +0530

    basic html code added

--- Reset-Password.php
+++ Reset-Password.php
@@ -1,1 +1,25 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reset Password</title>
+    <link rel="icon" type="image/png" href="images/icon.png" sizes="256x256">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+
+</head>
+<body>
+<div class="container">
+    <div class="row vh-100">
+        <div class="col-6 d-flex justify-content-center align-items-center">
+            <img src="images/reset.jpg" class="w-100" alt="Reset Password">
+        </div>
+        <div class="col-6">
+
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
