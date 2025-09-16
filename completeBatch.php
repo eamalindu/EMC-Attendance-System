8507ce1fcf44d2f6f4c3f51cb8b2637935e0f7e6
Malindu <eamalindu@gmail.com>
2025-09-16 14:55:36 +0530

    todo completed

--- completeBatch.php
+++ completeBatch.php
@@ -6,16 +6,23 @@ $loggedInUser = $_SESSION["username"];
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    //TODO: need to check whether batch is available and if already completed
     $batch = $_POST['batch'];
-
-    $stmt = $conn->prepare("UPDATE batch SET status = ? WHERE name = ?");
     $status = 'Completed';
-    $stmt->bind_param("ss", $status, $batch);
-    if ($stmt->execute()) {
-        echo "OK";
+
+    $stmtCheck = $conn->prepare("SELECT * FROM batch WHERE name  = ? and status != ?");
+    $stmtCheck->bind_param("ss", $batch, $status);
+    $stmtCheck->execute();
+    $result = $stmtCheck->get_result();
+    if ($result->num_rows > 0) {
+        $stmt = $conn->prepare("UPDATE batch SET status = ? WHERE name = ?");
+        $stmt->bind_param("ss", $status, $batch);
+        if ($stmt->execute()) {
+            echo "OK";
+        } else {
+            echo "failed: " . $stmt->error;
+        }
     } else {
-        echo "failed: " . $stmt->error;
+        echo "Batch not found or already completed.";
     }
 
 
