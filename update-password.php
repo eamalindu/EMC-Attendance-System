bcc1c5511b4dad909a78f671a78064c8d3eaa943
Malindu <eamalindu@gmail.com>
2025-09-17 20:41:55 +0530

    html boilerplate code added

--- update-password.php
+++ update-password.php
@@ -45,8 +45,34 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
 ?>
 
 <!-- HTML Form -->
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reset Password</title>
+    <link rel="icon" type="image/png" href="images/icon.png" sizes="256x256">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
+        * {
+            padding: 0;
+            margin: 0;
+            font-family: 'Poppins', sans-serif !important;
+        }
+    </style>
+    <body>
 <form method="POST">
     <input type="password" name="password" placeholder="New Password" required>
     <input type="password" name="confirm" placeholder="Confirm Password" required>
     <button type="submit">Reset Password</button>
 </form>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q"
+        crossorigin="anonymous"></script>
+<script src="customModal_V2/resources/js/customModal@2.0.min.js"></script>
+</body>
+</html>
