868f845867a6d1733d5f501dcdcaf76098058dbb
Malindu <eamalindu@gmail.com>
2025-09-17 15:13:04 +0530

    new function implemented

--- mailTemplate.php
+++ mailTemplate.php
@@ -44,9 +44,35 @@ function sendWelcomeMail($email,$name,$username)
     }
 }
 
-function sendPasswordResetMail()
+function sendPasswordResetMail($email,$link)
 {
+    $mail = new PHPMailer(true);
 
+    try {
+        $mail->isSMTP();
+        $mail->Host       = 'smtp.gmail.com';
+        $mail->SMTPAuth   = true;
+        $mail->Username   = 'finalproject.eamalindu@gmail.com';
+        $mail->Password   = 'ludfsvhjbybjhnth';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port       = 465;
+
+        $mail->setFrom('finalproject.eamalindu@gmail.com', 'EMC-NIT Attendance System');
+        $mail->addAddress($email);
+
+        // 1. Load the HTML template
+        $htmlBody = file_get_contents(__DIR__ . '/passwordReset.html');
+
+        $mail->isHTML(true);
+        $mail->Subject = 'Password Reset';
+        $mail->Body    = $htmlBody;
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        $mail->send();
+        echo 'OK';
+    } catch (Exception $e) {
+        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    }
 }
 
 //example
