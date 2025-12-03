2a6f60ae5612e8c91827d4ae0ae2e38a7ca734df
Malindu <eamalindu@gmail.com>
2025-12-03 09:21:56 +0530

    basic boilerplate codes added and form implemented

--- upload-students.php
+++ upload-students.php
@@ -1,1 +1,36 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Manage Batches</title>
+    <link rel="icon" type="image/png" href="images/icon.png" sizes="256x256">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/aquawolf04/font-awesome-pro@5cd1511/css/all.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.3.2/css/dataTables.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.3.2/css/dataTables.bootstrap5.min.css">
+    <link rel="stylesheet" href="css/all.css">
+    <link rel="stylesheet" href="css/loader.css">
+</head>
+<body>
+<div class="preloader w-100 min-vh-100">
+    <div class="loader"></div>
+</div>
+<div class="container p-0 py-5">
+    <h2 class="mb-3">Upload Students</h2>
+
+    <form action="#" type="post" enctype="multipart/form-data">
+        <input id="fileSelect" type="file" accept=".csv, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel" />
+        <button type="button" class="btn d-block mt-3 btn-primary w-25">Upload Students</button>
+    </form>
+
+</div>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q"
+        crossorigin="anonymous"></script>
+<script src="customModal_V2/resources/js/customModal@2.0.min.js"></script>
+<script src="js/upload-students.js"></script>
+<script src="js/loader.js"></script>
+</html>
